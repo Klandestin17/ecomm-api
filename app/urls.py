b1cb4fb94c42bf1fa9a6b72a3c59800b98f495e6
Rendi <elderdev17@gmail.com>
2024-11-16 17:32:45 +0800

    add wishlist sevice

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-
-    path('api/product/', include('core.urls'))
+    path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+    path("api/product/", include("core.urls")),
+    path("api/wishlist/", include("wishlist.urls")),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from wishlist import models, serializers
 
-# Create your views here.
+
+class GetWishlist(generics.ListAPIView):
+    serializer_class = serializers.WishlistSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return models.Wishlist.objects.filter(userId=self.request.user)
+
+
+class ToggleWishlist(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_id = request.id
+        product_id = request.query_params.get("id")
+
+        if not user_id or not product_id:
+            return Response(
+                {"message": "Invalid request a user id and product id is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            product = models.Product.objects.get(id=product_id)
+        except models.Product.DoesNotExist:
+            return Response(
+                {"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        wishlist_item, created = models.Wishlist.objects.get_or_create(
+            userId=request.user, product=product
+        )
+        if created:
+            return Response(
+                {"message": "Product added to wishlist"}, status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                {"message": "Product removed from wishlist"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
